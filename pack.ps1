9a181f4c2af55a2a3ca3812ab2784726f4089cf9
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-04-25 15:15:10 +0200

    zip job

--- pack.ps1
+++
@@ -1,7 +0,0 @@
-$TargetZipFileName = "~\Downloads\$(Split-Path -Path $PSScriptRoot -Leaf).zip"
-$compress = @{
-  Path             = "$PSScriptRoot\README.md", "$PSScriptRoot\CHANGELOG.md", "$PSScriptRoot\manifest.json", "$PSScriptRoot\icon.png", "$PSScriptRoot\config", "$PSScriptRoot\plugins"
-  CompressionLevel = "Fastest"
-  DestinationPath  = $TargetZipFileName
-}
-Compress-Archive @compress -Force
