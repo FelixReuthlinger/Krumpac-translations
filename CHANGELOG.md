d3a9b01170b338ebf797d9a9f97596efb43eef45
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-04-26 21:20:54 +0200

    .3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+ * 1.0.3 -> russian language updated
  * 1.0.2 -> fix the fix
  * 1.0.1 -> paths fixed, changed from copy to move
  * 1.0.0 -> first repost
---
+++ clone_from_englisch.ps1
@@ -0,0 +1,34 @@
+# $TargetZipFileName = "~\Downloads\$(Split-Path -Path $PSScriptRoot -Leaf).zip"
+# $compress = @{
+#   Path             = "$PSScriptRoot\README.md", "$PSScriptRoot\CHANGELOG.md", "$PSScriptRoot\manifest.json", "$PSScriptRoot\icon.png", "$PSScriptRoot\config", "$PSScriptRoot\plugins"
+#   CompressionLevel = "Fastest"
+#   DestinationPath  = $TargetZipFileName
+# }
+# Compress-Archive @compress -Force
+
+$PluginsFolder = "plugins"
+$FilePrefixesAndFolders = @{
+  "Datys_Professions" = "Krumpac-Datys_Professions";
+  "Krumpac_Armors" = "Krumpac-Krumpac_Armors";
+  "Krumpac_Building_Pieces" = "Krumpac-Krumpac_Building_Pieces";
+  "Krumpac_Dungeon_Meadows" = "Krumpac-Krumpac_Dungeon_Meadows"; 
+  "Krumpac_Reforge_Core" = "Krumpac-Krumpac_Reforge_Core"; 
+  "Krumpac_Sacrificing_Altars" = "Krumpac-Krumpac_Sacrificing_Altars";
+  "Krumpac_Tribe_Ornaments" = "Krumpac-Krumpac_Sacrificing_Altars";
+  "Krumpac_Weapon_Arsenal" = "Krumpac-Krumpac_Weapon_Arsenal"
+}
+$TranslationsSubFolder = "translations"
+$SourceLanguage = "Englisch"
+$FileSuffix = "yml"
+
+foreach ($kv in $FilePrefixesAndFolders.GetEnumerator()){
+  $CurrentFolder = "$PluginsFolder\" + $kv.Value + "\$TranslationsSubFolder"
+  $CurrentInputFile = $kv.Name + ".$SourceLanguage.$FileSuffix"
+  $CurrentInputFileRelative = "$CurrentFolder\$CurrentInputFile"
+  $CurrentInputFileAbsolute = "$PSScriptRoot\$CurrentInputFileRelative"
+  Write-Output($CurrentInputFileAbsolute)
+
+  Get-Content $CurrentInputFileAbsolute | Where-Object{ ! $_.StartsWith("#") } | Sort-Object | Get-Unique > $CurrentInputFileAbsolute
+}
+
+#Get-Content unsorted.txt | sort | get-unique > sorted.txt
